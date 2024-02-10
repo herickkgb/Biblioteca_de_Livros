ed32ebae86dfb822665ffc19d526bc892c5a68db
herickkgb <42222113+herickkgb@users.noreply.github.com>
2024-02-10 17:31:33 -0300

    commit inicial

---
+++ src/main/java/com/herick/book/BibliotecaDeLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.herick.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDeLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDeLivrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/herick/book/BibliotecaDeLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.herick.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDeLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
