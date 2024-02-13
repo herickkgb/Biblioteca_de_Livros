3354da81c284b25d6f5d9a9540c27846bf77f6c5
Herick alves pereira <42222113+herickkgb@users.noreply.github.com>
2024-02-13 14:16:12 -0300

    att exceptions

--- src/main/java/com/herick/book/exceptions/ResourceExceptionsHandler.java
+++ src/main/java/com/herick/book/exceptions/ResourceExceptionsHandler.java
@@ -1,7 +1,6 @@
 package com.herick.book.exceptions;
 
 import javax.servlet.ServletRequest;
-
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -14,39 +13,38 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class ResourceExceptionsHandler {
 
-	@ExceptionHandler(ObjectNotFoundExceptions.class)
-	private ResponseEntity<StandartError> bjectNotFoundExceptions(ObjectNotFoundExceptions e, ServletRequest request) {
-		StandartError error = new StandartError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
-				e.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-	}
+    @ExceptionHandler(ObjectNotFoundExceptions.class)
+    public ResponseEntity<StandartError> objectNotFoundExceptions(ObjectNotFoundExceptions e, ServletRequest request) {
+        StandartError error = new StandartError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+                e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 
-	@ExceptionHandler(EmptyResultDataAccessException.class)
-	private ResponseEntity<StandartError> emptyResultDataAccessException(EmptyResultDataAccessException ex,
-			ServletRequest request) {
-		StandartError error = new StandartError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
-				"Não existe nenhuma entidade de classe 'Categoria' com id 6!");
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
-	}
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<StandartError> emptyResultDataAccessException(EmptyResultDataAccessException ex,
+            ServletRequest request) {
+        StandartError error = new StandartError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+                "Recurso não encontrado ou já foi removido.");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
 
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	private ResponseEntity<StandartError> dataIntegrityViolationException(DataIntegrityViolationException ex,
-			ServletRequest request) {
-		StandartError error = new StandartError(System.currentTimeMillis(), HttpStatus.CONFLICT.value(),
-				"Categoria não pode ser deletada! Possui livros associados");
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
-	}
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandartError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+            ServletRequest request) {
+        StandartError error = new StandartError(System.currentTimeMillis(), HttpStatus.CONFLICT.value(),
+                "Violação de integridade de dados. Detalhes: " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
+    }
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	private ResponseEntity<StandartError> dataIntegrityViolationException(MethodArgumentNotValidException ex,
-			ServletRequest request) {
-		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.CONFLICT.value(),
-				"Erro na validação dos campos");
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> methodArgumentNotValidException(MethodArgumentNotValidException ex,
+            ServletRequest request) {
+        ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+                "Erro na validação dos campos");
 
-		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
-			error.addErrors(x.getField(), x.getDefaultMessage() );
-		}
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(error);
-	}
-
+        for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+            error.addErrors(x.getField(), x.getDefaultMessage());
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+    }
 }
